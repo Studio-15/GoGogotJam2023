c50e2fd6a9c2be4eb49fd0c38aba0a5a56e5674a
Saftevand <m010795@hotmail.com>
2023-05-06 17:56:04 +0200

    Initial Level

--- src/Player/player.gd
+++ src/Player/player.gd
@@ -1,12 +1,12 @@
 extends CharacterBody2D
 
-var move_speed = 10000
+var move_speed = 300
 
 
 func _physics_process(delta: float) -> void:
 	var velocity_x = Input.get_action_raw_strength("move_right") - Input.get_action_raw_strength("move_left")
 	var velocity_y = Input.get_action_raw_strength("move_down") - Input.get_action_raw_strength("move_up")
 
-	velocity = Vector2(velocity_x, velocity_y) * move_speed * delta
+	velocity = Vector2(velocity_x, velocity_y) * move_speed
 
 	move_and_slide()
