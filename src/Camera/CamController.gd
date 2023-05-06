e6c99e13de2aed11e01a6971980eda761b6dc86d
Saftevand <m010795@hotmail.com>
2023-05-06 17:56:42 +0200

    Removed old cam

--- src/Camera/CamController.gd
+++
@@ -1,8 +0,0 @@
-extends Camera2D
-
-
-@onready var player: CharacterBody2D = get_parent().get_node("Player")
-
-
-#func _physics_process(delta: float) -> void:
-#	global_position = Vector2(lerp(global_position.x, player.global_position.x, 0.05), lerp(global_position.y, player.global_position.y, 0.05))
