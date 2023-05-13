0bd9c770b69629f1f8e7f55be73a40efaa7de104
Jonas Holm <Jonas_holm15@hotmail.com>
2023-05-13 11:45:01 +0200

    Enemy types

--- src/Enemies/enemy.gd
+++ src/Enemies/enemy.gd
@@ -3,9 +3,23 @@ extends CharacterBody2D
 
 class_name Enemy
 
+@onready var nav_agent = $NavigationAgent2D
+@onready var rng = RandomNumberGenerator.new()
+@onready var stats: entity_stats = $Stats
 
+var target: CharacterBody2D
+var next_idle_location: int = 0
+var idle_points: Array = []
 var gale_force_strength: float
 var build_timer: Timer = Timer.new()
+var pause: bool = false
+var current_state: Enums.ENEMY_STATE = Enums.ENEMY_STATE.IDLE:
+	set(val):
+		current_state = val
+		if val == Enums.ENEMY_STATE.IDLE:
+			nav_agent.target_desired_distance = 10
+		else:
+			nav_agent.target_desired_distance = 135
 
 
 func _super() -> void:
@@ -42,3 +56,23 @@ func _on_build_trap():
 	var trap_size = $Sprite2D.texture.get_size()
 	trap.position = position + Vector2(trap_size.x * [-1, 1].pick_random(), trap_size.y * [-1, 1].pick_random())
 	get_parent().add_child(trap)
+
+
+func _on_navigation_agent_2d_target_reached() -> void:
+	if pause:
+		return
+	if current_state == Enums.ENEMY_STATE.IDLE:
+		var randint = rng.randi_range(0, len(idle_points)-1)
+		next_idle_location = randint
+	elif current_state in [Enums.ENEMY_STATE.CHASING, Enums.ENEMY_STATE.ENRAGED]:
+		if !is_instance_valid(target):
+			return
+		pause = true
+		target.take_damage(stats.ATTACK)
+		await get_tree().create_timer(stats.ATTACK_COOLDOWN).timeout
+		velocity = Vector2.ZERO
+		pause = false
+		# Not sure if this is the intended behaviour we want,but lets test it 
+		# out -> It goes full berserk mode until it hits the player, and then 
+		# it can be distracted again.
+		current_state = Enums.ENEMY_STATE.CHASING 
--- src/Enemies/test_enemy.gd
+++ src/Enemies/test_enemy.gd
@@ -1,20 +1,6 @@
 extends Enemy
 
 
-@onready var stats: entity_stats = $Stats
-@onready var nav_agent = $NavigationAgent2D
-@onready var rng = RandomNumberGenerator.new()
-var target: CharacterBody2D
-var pause: bool = false
-var current_state: Enums.ENEMY_STATE = Enums.ENEMY_STATE.IDLE:
-	set(val):
-		current_state = val
-		if val == Enums.ENEMY_STATE.IDLE:
-			nav_agent.target_desired_distance = 10
-		else:
-			nav_agent.target_desired_distance = 135
-var idle_points: Array = []
-var next_idle_location: int = 0
 var enemy_type: Enums.ENEMY_TYPE = Enums.ENEMY_TYPE.RUNNER
 
 
@@ -85,24 +71,6 @@ func chase():
 	var next_loc = nav_agent.get_next_path_position()
 	var new_velocity = (next_loc - global_position).normalized() * (stats.MOVE_SPEED / (1 if current_state != Enums.ENEMY_STATE.IDLE else 8))
 	nav_agent.set_velocity(new_velocity)
-
-
-func _on_navigation_agent_2d_target_reached() -> void:
-	if pause:
-		return
-	if current_state == Enums.ENEMY_STATE.IDLE:
-		var randint = rng.randi_range(0, len(idle_points)-1)
-		next_idle_location = randint
-	elif current_state in [Enums.ENEMY_STATE.CHASING, Enums.ENEMY_STATE.ENRAGED]:
-		if !is_instance_valid(target):
-			return
-		pause = true
-		target.take_damage(stats.ATTACK)
-		await get_tree().create_timer(stats.ATTACK_COOLDOWN).timeout
-		print(stats.ATTACK_COOLDOWN)
-		velocity = Vector2.ZERO
-		pause = false
-		current_state = Enums.ENEMY_STATE.CHASING # Not sure if this is the intended behaviour we want, but lets test it out -> It goes full berserk mode until it hits the player, and then it can be distracted again.
 
 
 func _on_detection_area_body_entered(body: Node2D) -> void:
